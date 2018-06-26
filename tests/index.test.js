47c9de4faf4e2979f3f73b0271f72dca4973a3a4
aps120797 <aps.120797@gmail.com>
2018-06-26 18:00:29 +0530

    Added sample test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,2 +1,6 @@
 // Tests use Jest by default.  
 // To read more about testing with Probot, visit https://probot.github.io/docs/testing/
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3)
+})
